d97042236ce3e551dd99bc6a7f3f8c57488b8dbc
MAYOHR\erica_lh_lee <erica_lh_lee@mayohr.com>
2024-08-20 22:38:48 +0800

    Erica Tennis

--- TennisKata/Program.cs
+++ TennisKata/Program.cs
@@ -8,6 +8,19 @@ return;
 static void Main()
 {
     var tennis = new Tennis("Sam1", "Sam2");
+    tennis.PlayerOneScore();
+    tennis.PlayerOneScore();
+    tennis.PlayerOneScore();
+    tennis.PlayerTwoScore();
+    tennis.PlayerTwoScore();
+    tennis.PlayerTwoScore();
+    tennis.PlayerTwoScore();
+    tennis.PlayerOneScore();
+    tennis.PlayerOneScore();
+    tennis.PlayerTwoScore();
+    tennis.PlayerTwoScore();
+    tennis.PlayerTwoScore();
+
 
     var score = tennis.Score();
     Console.WriteLine(score);
--- TennisKata/Tennis.cs
+++ TennisKata/Tennis.cs
@@ -16,11 +16,79 @@ public class Tennis
     }
     public string Score()
     {
+
         //1. 輸出必須為網球分數 例：1:0 => Fifteen Love 
         //2. 分數表達必須是文字例如Fifteen Forty 而非15：40 
         //3. 賽末點時 輸出為 Player Name Adv, 例：Sam Adv
         //4. 勝出時 輸出為 Player Name Win, 例：Sam Win
-        return "Love All";
+
+        // 平分時
+        if (_playerOneScore == _playerTwoScore)
+        {
+            switch (_playerOneScore)
+            {
+                case 0:
+                    return "Love All";
+                    break;
+                case 1:
+                    return "Fifteen All";
+                    break;
+                case 2:
+                    return "Thirty All";
+                    break;
+                default:
+                    return "Deuce";
+                    break;
+            };
+        };
+        // 進到deuce後
+        if (_playerOneScore >= 4 || _playerTwoScore >= 4)
+        {
+            int scoreDifference = _playerOneScore - _playerTwoScore;
+            if (scoreDifference == 1) { return $"{_playerOneName} Adv"; }
+            if (scoreDifference == -1) { return $"{_playerTwoName} Adv"; }
+            if (scoreDifference == 2) { return $"{_playerOneName} Win"; }
+            if (scoreDifference == -2) { return $"{_playerTwoName} Win"; }
+        }
+
+        // 計分
+        string score1 = getScore(_playerOneScore,_playerOneName);
+        string score2 = getScore(_playerTwoScore, _playerTwoName);
+        if (_playerOneScore == 4)
+        {
+            return $"{_playerOneName} Win";
+        }
+        else if (_playerTwoScore == 4)
+        {
+            return $"{_playerTwoName} Win";
+        }
+        else 
+        { 
+            return $"{score1} {score2}";
+        }
+
+    }
+    // 不同比分時所顯示的字串
+    public string getScore(int score, string name) 
+    {
+        switch (score) {
+            case 0:
+                return "Love";
+                break;
+            case 1:
+                return "Fifteen";
+                break;
+            case 2:
+                return "Thirty";
+                break;
+            case 3:
+                return "Forty";
+                break;
+            default:
+                return "";
+
+
+        }
     }
 
     public void PlayerOneScore()
