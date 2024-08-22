8a4a1b6b77159d7154aadb778b853d34ec8b96fd
Arthur <arthur_liu@mayohr.com>
2024-08-23 01:06:38 +0800

    refactor: Rename And Adjust Method To Improve Readability

--- UnitTest/ScoreTests.cs
+++ UnitTest/ScoreTests.cs
@@ -6,24 +6,24 @@ namespace UnitTest;
 
 public class ScoreTests
 {
-    private Player _player1;
-    private Player _player2;
+    private Player _scoringPlayer;
+    private Player _opponentPlayer;
     private Scorer _scorer;
 
 
     public ScoreTests()
     {
-        _player1 = new Player("Heng");
-        _player2 = new Player("Arthur");
+        _scoringPlayer = new Player("Heng");
+        _opponentPlayer = new Player("Arthur");
         _scorer = new Scorer();
     }
 
     [Fact]
-    public void PlayerAddScoreOnce()
+    public void GivenPlayersScoreLessThanThree_WhenPlayerScores_ThenScoreShouldIncrease()
     {
-        GivenPlayersZeroScore();
-        WhenPlayerScoring(_player1, 1);
-        ThenPlayerScoreShouldBe(_player1, 1);
+        GivenPlayersScore(0,0);
+        WhenPlayerScores(_scoringPlayer);
+        ThenPlayerScoreShouldBe(_scoringPlayer, 1);
     }
 
     private void ThenPlayerScoreShouldBe(Player player, int expectedScore)
@@ -31,19 +31,15 @@ public class ScoreTests
         player.Score.Should().Be(expectedScore);
     }
 
-    private void WhenPlayerScoring(Player scoringPlayer, int scoreIncrement)
+    private void WhenPlayerScores(Player scoringPlayer)
     {
-        var opponentPlayer = scoringPlayer.Equals(_player1) ? _player2 : _player1;
-
-        for (var i = 0; i < scoreIncrement; i++)
-        {
-            _scorer.AddPlayerScore(scoringPlayer, opponentPlayer);
-        }
+        var opponentPlayer = scoringPlayer.Equals(_scoringPlayer) ? _opponentPlayer : _scoringPlayer;
+        _scorer.AddPlayerScore(scoringPlayer, opponentPlayer);
     }
 
-    private void GivenPlayersZeroScore()
+    private void GivenPlayersScore(int scoringPlayerScore,int opponentPlayerScore)
     {
-        _player1.Score = 0;
-        _player2.Score = 0;
+        _scoringPlayer.Score = scoringPlayerScore;
+        _opponentPlayer.Score = opponentPlayerScore;
     }
 }
