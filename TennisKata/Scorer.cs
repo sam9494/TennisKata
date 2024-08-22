21c2dbbe443580fc272f5c7b1f87499ae1cc08d0
Arthur <arthur_liu@mayohr.com>
2024-08-23 01:43:11 +0800

    feat: Adjust AddPlayerScore Method To Pass Test GivenScoringPlayerScoreMoreThanThreeAndLeadMoreThanTwo

--- TennisKata/Scorer.cs
+++ TennisKata/Scorer.cs
@@ -4,6 +4,16 @@ public class Scorer
 {
     public void AddPlayerScore(Player scoringPlayer, Player opponentPlayer)
     {
+        var isLeadMoreThenOne = Math.Abs(scoringPlayer.Score - opponentPlayer.Score) > 1;
+        var isAnyPlayerScoreHigherThenThree = scoringPlayer.Score > 3 || opponentPlayer.Score > 3;
+        
+        if (isLeadMoreThenOne && isAnyPlayerScoreHigherThenThree)
+        {
+            var leadPlayer = scoringPlayer.Score > opponentPlayer.Score  ? scoringPlayer : opponentPlayer;
+            Console.WriteLine($"Player score did not increase; {leadPlayer.Name} has won.");
+            return;
+        }
+        
         scoringPlayer.Score++;
     }
 }
