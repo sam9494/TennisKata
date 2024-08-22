850d6ad6c5463775fc8db90d79c66341e2f999ee
MAYOHR\kai_kang <kai_kang@mayohr.com>
2024-08-23 00:04:05 +0800

    feature: 網球計分邏輯

--- TennisKata/Program.cs
+++ TennisKata/Program.cs
@@ -9,6 +9,16 @@ static void Main()
 {
     var tennis = new Tennis("Sam1", "Sam2");
 
+    tennis.PlayerOneScore();
+    //tennis.PlayerTwoScore();
+    tennis.PlayerOneScore();
+    // tennis.PlayerTwoScore();
+    tennis.PlayerOneScore();
+    // tennis.PlayerTwoScore();
+    tennis.PlayerOneScore();
+    // tennis.PlayerTwoScore();
+    // tennis.PlayerTwoScore();
+    // tennis.PlayerTwoScore();
     var score = tennis.Score();
     Console.WriteLine(score);
 }
--- TennisKata/Tennis.cs
+++ TennisKata/Tennis.cs
@@ -14,12 +14,58 @@ public class Tennis
         _playerOneScore = 0;
         _playerTwoScore = 0;
     }
+    
+
     public string Score()
     {
         //1. 輸出必須為網球分數 例：1:0 => Fifteen Love 
         //2. 分數表達必須是文字例如Fifteen Forty 而非15：40 
         //3. 賽末點時 輸出為 Player Name Adv, 例：Sam Adv
         //4. 勝出時 輸出為 Player Name Win, 例：Sam Win
+        Dictionary<int,string> scoreName = new Dictionary<int, string>();
+        scoreName.Add(0, "Love");
+        scoreName.Add(1, "Fifteen");
+        scoreName.Add(2, "Thirty");
+        scoreName.Add(3, "Forty");
+
+        var playerOneScore = _playerOneScore;
+        var playerTwoScore = _playerTwoScore;
+
+        if (playerOneScore == playerTwoScore && scoreName.ContainsKey(playerOneScore) && playerOneScore < 3)
+        {
+            return scoreName[playerTwoScore] + " ALL";
+        }
+        
+        if (playerOneScore == playerTwoScore)
+        {
+            return "deuce";
+        }
+        
+        if (playerOneScore >= 4 && (playerOneScore-playerTwoScore >= 2))
+        {
+            return _playerOneName + " Win";
+        }
+        
+        if (playerTwoScore >= 4 && (playerTwoScore-playerOneScore >= 2))
+        {
+            return _playerTwoName + " Win";
+        }
+
+        if (playerOneScore >= 3 && (playerOneScore-playerTwoScore >= 1))
+        {
+            return _playerOneName + " Adv";
+        }
+        
+        if (playerTwoScore >= 3 && (playerTwoScore-playerOneScore >= 1))
+        {
+            return _playerTwoName + " Adv";
+        }
+
+        if (scoreName.TryGetValue(playerOneScore, out var score1) && scoreName.TryGetValue(playerTwoScore, out var score2))
+        {
+            return score1 +" "+ score2;
+        }
+
         return "Love All";
     }
 
