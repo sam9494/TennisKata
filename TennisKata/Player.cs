7c6557ce6f664b38df7258e02bc1253b0ec1f251
Arthur <arthur_liu@mayohr.com>
2024-08-22 08:41:20 +0800

    feat: Player Override Equals For UnitTest

--- TennisKata/Player.cs
+++ TennisKata/Player.cs
@@ -4,4 +4,9 @@ public class Player(string name)
 {
     public string Name { get; } = name;
     public int Score { get; set; } = 0;
+    public override bool Equals(object? o)
+    {
+        if (o is Player player) return this.Name == player.Name;
+        return false;
+    }
 }
