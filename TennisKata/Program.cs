e52155094cabbec6cb65499843f391fa8f0f1635
MAYOHR\bonnie_kao <bonnie_kao@mayohr.com>
2024-08-23 13:40:35 +0800

    feat: first version

--- TennisKata/Program.cs
+++ TennisKata/Program.cs
@@ -8,6 +8,15 @@ return;
 static void Main()
 {
     var tennis = new Tennis("Sam1", "Sam2");
+    tennis.PlayerTwoScore();
+    tennis.PlayerTwoScore();
+    tennis.PlayerOneScore();
+    tennis.PlayerOneScore();
+    tennis.PlayerOneScore();
+    tennis.PlayerTwoScore();
+    tennis.PlayerTwoScore();
+    tennis.PlayerOneScore();
+    tennis.PlayerOneScore();
 
     var score = tennis.Score();
     Console.WriteLine(score);
--- TennisKata/Tennis.cs
+++ TennisKata/Tennis.cs
@@ -3,9 +3,9 @@
 public class Tennis
 {
     private readonly string _playerOneName;
+    private readonly string _playerTwoName;
     private int _playerOneScore;
     private int _playerTwoScore;
-    private readonly string _playerTwoName;
 
     public Tennis(string playerOneName, string playerTwoName)
     {
@@ -20,7 +20,54 @@ public class Tennis
         //2. 分數表達必須是文字例如Fifteen Forty 而非15：40 
         //3. 賽末點時 輸出為 Player Name Adv, 例：Sam Adv
         //4. 勝出時 輸出為 Player Name Win, 例：Sam Win
-        return "Love All";
+        if (_playerOneScore == 0 && _playerTwoScore == 0)
+        {
+            return "Love All";
+        }
+        else if (_playerOneScore >= 4 && _playerOneScore - _playerTwoScore >= 2)
+        {
+            return $"{_playerOneName} Win";
+        }
+        else if (_playerTwoScore >= 4 && _playerTwoScore - _playerOneScore >= 2)
+        {
+            return $"{_playerTwoName} Win";
+        }
+        else if (_playerOneScore >= 3 && _playerTwoScore >= 3)
+        {
+            if (_playerOneScore == _playerTwoScore)
+            {
+                return "Deuce";
+            }
+            else if (_playerOneScore > _playerTwoScore)
+            {
+                return $"{_playerOneName} Adv";
+            }
+            else
+            {
+                return $"{_playerTwoName} Adv";
+            }
+        }
+        else
+        {
+            return $"{GetScoreDescription(_playerOneScore)} {GetScoreDescription(_playerTwoScore)}";
+        }
+    }
+
+    public string GetScoreDescription(int score)
+    {
+        switch (score)
+        {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return "Error";
+        }
     }
 
     public void PlayerOneScore()
