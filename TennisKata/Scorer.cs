db3e1d31fc8de5e8836e1fd0d2ffb49e58ff3fde
Arthur <arthur_liu@mayohr.com>
2024-08-24 09:53:41 +0800

    refactor: Adjust Variable To Improve Readability

--- TennisKata/Scorer.cs
+++ TennisKata/Scorer.cs
@@ -2,12 +2,13 @@ namespace TennisKata;
 
 public class Scorer
 {
-    public void AddPlayerScore(Player scoringPlayer, Player opponentPlayer)
+    public void AddPlayerScore(Player scoringPlayer, Player opponentPlayer)//簽章要不要放 opponentPlayer? 給了是告訴讀者加分邏輯會受到對手分數影響
     {
-        var isLeadMoreThenOne = Math.Abs(scoringPlayer.Score - opponentPlayer.Score) > 1;
-        var isAnyPlayerScoreHigherThenThree = scoringPlayer.Score > 3 || opponentPlayer.Score > 3;
+        var leadScore = Math.Abs(scoringPlayer.Score - opponentPlayer.Score);
+        var highestScore = Math.Max(scoringPlayer.Score, opponentPlayer.Score);
+        var isGameOver = leadScore > 1 && highestScore > 3;
         
-        if (isLeadMoreThenOne && isAnyPlayerScoreHigherThenThree)
+        if (isGameOver)
         {
             var leadPlayer = scoringPlayer.Score > opponentPlayer.Score  ? scoringPlayer : opponentPlayer;
             Console.WriteLine($"Player score did not increase; {leadPlayer.Name} has won.");
