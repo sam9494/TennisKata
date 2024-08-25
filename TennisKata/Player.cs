6efd7a410203279e898633b576b496fd03c1f525
Arthur <arthur_liu@mayohr.com>
2024-08-25 22:26:42 +0800

    refactor: Remove Scorer After Add CheckGameOver

--- TennisKata/Player.cs
+++ TennisKata/Player.cs
@@ -4,9 +4,4 @@ public class Player(string name)
 {
     public string Name { get; } = name;
     public int Score { get; set; } = 0;
-    public override bool Equals(object? o)
-    {
-        if (o is Player player) return this.Name == player.Name;
-        return false;
-    }
 }
