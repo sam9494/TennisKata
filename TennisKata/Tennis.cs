31744898d760362d9d3671a08977459533cc6f58
sam_chong <sam_chong@mayohr.com>
2024-09-02 09:06:53 +0800

    complete tennis kata by TDD

--- TennisKata/Tennis.cs
+++ TennisKata/Tennis.cs
@@ -7,6 +7,14 @@ public class Tennis
     private int _playerTwoScore;
     private readonly string _playerTwoName;
 
+    private readonly Dictionary<int, string> _scoreLookup = new Dictionary<int, string>()
+    {
+        { 0, "Love" },
+        { 1, "Fifteen" },
+        { 2, "Thirty" },
+        { 3, "Forty" },
+    };
+
     public Tennis(string playerOneName, string playerTwoName)
     {
         _playerOneName = playerOneName;
@@ -14,23 +22,75 @@ public class Tennis
         _playerOneScore = 0;
         _playerTwoScore = 0;
     }
+
     public string Score()
     {
         //1. 輸出必須為網球分數 例：1:0 => Fifteen Love 
         //2. 分數表達必須是文字例如Fifteen Forty 而非15：40 
         //3. 賽末點時 輸出為 Player Name Adv, 例：Sam Adv
         //4. 勝出時 輸出為 Player Name Win, 例：Sam Win
-        return "Love All";
+        return IsSameScore() ? IsDeuce() ? Deuce() : SameScore() :
+            IsGamePoint() ? IsAdv() ? AdvState() : Win() : ScoreLookup();
+    }
+
+    private string ScoreLookup()
+    {
+        return $"{_scoreLookup[_playerOneScore]} {_scoreLookup[_playerTwoScore]}";
+    }
+
+    private string Win()
+    {
+        return $"{AdvPlayer()} Win";
+    }
+
+    private string AdvState()
+    {
+        return $"{AdvPlayer()} Adv";
+    }
+
+    private string AdvPlayer()
+    {
+        var advPlayer = _playerOneScore > _playerTwoScore ? _playerOneName : _playerTwoName;
+        return advPlayer;
+    }
+
+    private bool IsAdv()
+    {
+        return Math.Abs(_playerOneScore - _playerTwoScore) == 1;
+    }
+
+    private bool IsGamePoint()
+    {
+        return _playerOneScore > 3 || _playerTwoScore > 3;
+    }
+
+    private string SameScore()
+    {
+        return $"{_scoreLookup[_playerOneScore]} All";
+    }
+
+    private static string Deuce()
+    {
+        return "Deuce";
+    }
+
+    private bool IsDeuce()
+    {
+        return _playerTwoScore >= 3;
+    }
+
+    private bool IsSameScore()
+    {
+        return _playerOneScore == _playerTwoScore;
     }
 
     public void PlayerOneScore()
     {
         _playerOneScore++;
     }
-    
+
     public void PlayerTwoScore()
     {
-        _playerTwoScore++; 
+        _playerTwoScore++;
     }
-
 }
--- TennisKataTest/Test.cs
+++ TennisKataTest/Test.cs
@@ -1,9 +1,159 @@
+using FluentAssertions;
+using TennisKata;
+
 namespace TennisKataTest;
 
 public class Test
 {
-    [Fact]
-    public void Test1()
+    private readonly Tennis _tennis;
+
+    public Test()
     {
+        _tennis = new Tennis("Sam", "Bryant");
+    }
+
+    [Fact]
+    public void LoveAll()
+    {
+        var actual = _tennis.Score();
+        actual.Should().Be("Love All");
+    }
+
+    [Fact]
+    public void FifteenLove()
+    {
+        GivenPlayerOneScore(1);
+        var actual = _tennis.Score();
+        actual.Should().Be("Fifteen Love");
+    }
+
+    [Fact]
+    public void ThirtyLove()
+    {
+        GivenPlayerOneScore(2);
+        var actual = _tennis.Score();
+        actual.Should().Be("Thirty Love");
+    }
+
+    [Fact]
+    public void FortyLove()
+    {
+        GivenPlayerOneScore(3);
+        var actual = _tennis.Score();
+        actual.Should().Be("Forty Love");
+    }
+
+    [Fact]
+    public void LoveFifteen()
+    {
+        GivenPlayerTwoScore(1);
+        var actual = _tennis.Score();
+        actual.Should().Be("Love Fifteen");
+    }
+
+    [Fact]
+    public void LoveThirty()
+    {
+        GivenPlayerTwoScore(2);
+        var actual = _tennis.Score();
+        actual.Should().Be("Love Thirty");
+    }
+
+    [Fact]
+    public void LoveForty()
+    {
+        GivenPlayerTwoScore(3);
+        var actual = _tennis.Score();
+        actual.Should().Be("Love Forty");
+    }
+
+    [Fact]
+    public void FifteenThirty()
+    {
+        GivenPlayerOneScore(1);
+        GivenPlayerTwoScore(2);
+        var actual = _tennis.Score();
+        actual.Should().Be("Fifteen Thirty");
+    }
+
+    [Fact]
+    public void FifteenAll()
+    {
+        GivenPlayerOneScore(1);
+        GivenPlayerTwoScore(1);
+        var actual = _tennis.Score();
+        actual.Should().Be("Fifteen All");
+    }
+
+    [Fact]
+    public void ThirtyAll()
+    {
+        GivenPlayerOneScore(2);
+        GivenPlayerTwoScore(2);
+        var actual = _tennis.Score();
+        actual.Should().Be("Thirty All");
+    }
+
+    [Fact]
+    public void Deuce()
+    {
+        GivenPlayerOneScore(3);
+        GivenPlayerTwoScore(3);
+        var actual = _tennis.Score();
+        actual.Should().Be("Deuce");
+    }
+
+    [Fact]
+    public void Player1Adv()
+    {
+        GivenPlayerOneScore(4);
+        GivenPlayerTwoScore(3);
+        var actual = _tennis.Score();
+        actual.Should().Be("Sam Adv");
+    }
+
+    [Fact]
+    public void Player2Adv()
+    {
+        GivenPlayerOneScore(3);
+        GivenPlayerTwoScore(4);
+        var actual = _tennis.Score();
+        actual.Should().Be("Bryant Adv");
+    }
+
+    [Fact]
+    public void PlayerOneWin()
+    {
+        GivenPlayerOneScore(5);
+        GivenPlayerTwoScore(3);
+        var actual = _tennis.Score();
+        actual.Should().Be("Sam Win");
+    }
+
+    [Fact]
+    public void PlayerTwoWin()
+    {
+        GivenPlayerOneScore(3);
+        GivenPlayerTwoScore(5);
+        var actual = _tennis.Score();
+        actual.Should().Be("Bryant Win");
+    }
+
+
+    private void GivenPlayerTwoScore(int times)
+    {
+        for (var i = 0; i < times; i++)
+        {
+            _tennis.PlayerTwoScore();
+        }
+    }
+
+
+    private void GivenPlayerOneScore(int times)
+    {
+        for (var i = 0; i < times; i++)
+        {
+            _tennis.PlayerOneScore();
+        }
     }
 }
